5ca928a710959be8a5c647d123969428f39d78df
Robert Alef <ralef7@gmail.com>
2015-01-27 16:42:32 -0600

    week3
    Updated some of the naming just to make the program clearer

--- divvy.py
+++ divvy.py
@@ -17,16 +17,16 @@ for i in range(x):
     if len(result):
         latitude = result['stationBeanList'][i]['latitude']
         longitude = result['stationBeanList'][i]['longitude']
-        stations = result['stationBeanList'][i]['stationName']
+        station_name = result['stationBeanList'][i]['stationName']
         bikes = result['stationBeanList'][i]['availableBikes']
-        station_distance_euclid = ((longitude + 87.600915)*(longitude + 87.600915))
-        static_distance_euclid = ((latitude - 41.793414)*(latitude-41.793414))
-        distance= math.sqrt(station_distance_euclid + static_distance_euclid)
+        longitude_euclid = ((longitude + 87.600915)*(longitude + 87.600915))
+        latitude_euclid = ((latitude - 41.793414)*(latitude-41.793414))
+        distance= math.sqrt(longitude_euclid + latitude_euclid)
         
         
     if distance < min_distance: 
         min_distance = distance
-        min_station = stations
+        min_station = station_name
         min_bikes = bikes
 print("The nearest station is: {0} and it currently has {1} bikes available".format(min_station, min_bikes))
             
